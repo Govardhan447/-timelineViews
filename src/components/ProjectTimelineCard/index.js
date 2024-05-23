a48868fe42c2bcb9f3e20de2542d3076570f3bb3
Govardhan447 <163231039+Govardhan447@users.noreply.github.com>
2024-05-23 15:09:46 +0530

    Update index.js

--- src/components/ProjectTimelineCard/index.js
+++ src/components/ProjectTimelineCard/index.js
@@ -4,23 +4,26 @@ import './index.css'
 const ProjectTimelineCard = props => {
   const {projectTimelineDetails} = props
 
-  const {title, projectTitle, description} = projectTimelineDetails
+  const {projectTitle, description} = projectTimelineDetails
   const {duration, imageUrl, projectUrl} = projectTimelineDetails
 
   return (
     <>
-      <div>
-        <h1>{title}</h1>
-      </div>
-      <div>
-        <div>
-          <h1>{projectTitle}</h1>
-          <div>
-            <AiFillCalendar />
-            <p>{duration}</p>
-          </div>
+      <img className="project-image" src={imageUrl} alt={projectTitle} />
+
+      <div className="title-duration-container">
+        <h1>{projectTitle}</h1>
+
+        <div className="duration-container">
+          <AiFillCalendar />
+          <p>{duration}</p>
         </div>
+      </div>
+      <div className="description-container">
         <p>{description}</p>
+        <a className="link" href={projectUrl}>
+          Visit
+        </a>
       </div>
     </>
   )
